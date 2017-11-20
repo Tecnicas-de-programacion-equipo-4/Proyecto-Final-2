ff7a2099381416cc2537800c745dca122a01bcbe
luis M <lumarsa1712@gmail.com>
2017-11-19 22:56:11 -0600

    Added temperature manager

---
+++ Models/TemperatureControl.py
@@ -0,0 +1,19 @@
+from urllib import request
+import json
+
+class TemperatureManager():
+    class Constants:
+        base_url = "http://api.openweathermap.org/data/2.5/weather?q=Mexico,MX&APPID=3da430edb8cb3e68ab0ad0f3719f41d8"
+        kelvin_temperature = 273.15
+    @classmethod
+    def get_temperature(cls):
+        try:
+            with request.urlopen(cls.Constants.base_url) as response:
+                data = response.read().decode()
+                json_data = json.loads(data)
+                variable_aux = json_data["main"]
+                temperature = float(variable_aux["temp"]) - cls.Constants.kelvin_temperature
+
+                return temperature
+        except Exception as error:
+            print('Error')
