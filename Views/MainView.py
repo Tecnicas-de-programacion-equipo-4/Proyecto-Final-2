6950ac0692d441d8277d1b95bfcadf46b647e0e3
Aidan Ortega <aidanortega@MacBook-Pro-de-Aidan.local>
2017-11-22 08:13:08 -0600

    Ther requiered commit by the system

--- Views/MainView.py
+++ Views/MainView.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, N, S, E, W, Label
 from Views.ToggleButton import ToggleButton
+from Views.KeypadView import KeypadView
 class MainView(Tk):
     class Constants:
         title = "Casa Inteligente"
@@ -17,30 +18,7 @@ class MainView(Tk):
         self.title(self.Constants.title)
         self.geometry(self.Constants.size())
 
-        self.__bedroom_one_label = Label(self, text="Room\nOne", relief = "sunken")
-        self.__bedroom_one_label.grid(row=0, column=0, sticky=self.Constants.center)
-        self.__bedroom_one_toogle = ToggleButton(self, "Bedroom One", action=self.__did_tap, tap_toggle_handler= tap_handler)
-        self.__bedroom_one_toogle.grid(row=1, column=0, sticky=self.Constants.center)
 
-        self.__bedroom_two_label = Label(self, text="Room\nTwo", relief = "sunken")
-        self.__bedroom_two_label.grid(row=0, column=1, sticky=self.Constants.center)
-        self.__bedroom_two_toogle = ToggleButton(self, "Bedroom Two", action=self.__did_tap, tap_toggle_handler= tap_handler)
-        self.__bedroom_two_toogle.grid(row=1, column=1, sticky=self.Constants.center)
-
-        self.__living_label = Label(self, text="Living\nroom", relief = "sunken")
-        self.__living_label.grid(row=0, column=2, sticky=self.Constants.center)
-        self.__living_toogle = ToggleButton(self, "Living room", action=self.__did_tap, tap_toggle_handler= tap_handler)
-        self.__living_toogle.grid(row=1, column=2, sticky=self.Constants.center)
-
-        self.__dining_label = Label(self, text="Dining\nroom", relief = "sunken")
-        self.__dining_label.grid(row=0, column=3, sticky=self.Constants.center)
-        self.__dining_toogle = ToggleButton(self, "Dining room", action=self.__did_tap, tap_toggle_handler= tap_handler)
-        self.__dining_toogle.grid(row=1, column=3, sticky=self.Constants.center)
-
-        self.__bathroom_label = Label(self, text="Bath\nroom", relief = "sunken")
-        self.__bathroom_label.grid(row=0, column=4, sticky=self.Constants.center)
-        self.__bathroom_toogle = ToggleButton(self, "Bathroom", action=self.__did_tap, tap_toggle_handler= tap_handler)
-        self.__bathroom_toogle.grid(row=1, column=4, sticky=self.Constants.center)
 
         self.grid_rowconfigure(0, weight=self.Constants.heigth//2)
         self.grid_rowconfigure(1, weight=self.Constants.heigth//2)
