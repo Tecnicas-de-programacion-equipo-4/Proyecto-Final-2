3f3a14ba73bd5565c202e95a0b66f87111611ab5
Aidan Ortega <aidanortega@MacBook-Pro-de-Aidan.local>
2017-11-21 22:14:24 -0600

    Now the ligths are working well

--- Views/MainView.py
+++ Views/MainView.py
@@ -1,6 +1,5 @@
 from tkinter import Tk, N, S, E, W, Label
 from Views.ToggleButton import ToggleButton
-
 class MainView(Tk):
     class Constants:
         title = "Casa Inteligente"
@@ -12,37 +11,36 @@ class MainView(Tk):
         def size(cls):
             return "{}x{}".format(cls.width, cls.heigth)
 
-    def __init__(self, tap_toogle_handler = None, tap_handler = None):
+    def __init__(self, toogle_handler = None, tap_handler = None):
         super().__init__()
-        self.__tap_toogle_handler = tap_toogle_handler
+        self.__toogle_handler = toogle_handler
         self.title(self.Constants.title)
         self.geometry(self.Constants.size())
 
         self.__bedroom_one_label = Label(self, text="Room\nOne", bg="red")
         self.__bedroom_one_label.grid(row=0, column=0, sticky=self.Constants.center)
-        self.__bedroom_one_toogle = ToggleButton(self, "Bedroom One", action = self.__did_tap, tap_toggle_handler=tap_handler)
+        self.__bedroom_one_toogle = ToggleButton(self, "Bedroom One", action=self.__did_tap, tap_toggle_handler= tap_handler)
         self.__bedroom_one_toogle.grid(row=1, column=0, sticky=self.Constants.center)
 
         self.__bedroom_two_label = Label(self, text="Room\nTwo", bg="yellow")
         self.__bedroom_two_label.grid(row=0, column=1, sticky=self.Constants.center)
-        self.__bedroom_two_toogle = ToggleButton(self, "Bedroom Two", action = self.__did_tap, tap_toggle_handler=tap_handler)
+        self.__bedroom_two_toogle = ToggleButton(self, "Bedroom Two", action=self.__did_tap, tap_toggle_handler= tap_handler)
         self.__bedroom_two_toogle.grid(row=1, column=1, sticky=self.Constants.center)
 
         self.__living_label = Label(self, text="Living\nroom", bg="blue", fg="white")
         self.__living_label.grid(row=0, column=2, sticky=self.Constants.center)
-        self.__living_toogle = ToggleButton(self, "Living room", action = self.__did_tap, tap_toggle_handler=tap_handler)
+        self.__living_toogle = ToggleButton(self, "Living room", action=self.__did_tap, tap_toggle_handler= tap_handler)
         self.__living_toogle.grid(row=1, column=2, sticky=self.Constants.center)
 
         self.__dining_label = Label(self, text="Dining\nroom", bg="green")
         self.__dining_label.grid(row=0, column=3, sticky=self.Constants.center)
-        self.__dining_toogle = ToggleButton(self,"Dining room", action = self.__did_tap, tap_toggle_handler=tap_handler)
+        self.__dining_toogle = ToggleButton(self, "Dining room", action=self.__did_tap, tap_toggle_handler= tap_handler)
         self.__dining_toogle.grid(row=1, column=3, sticky=self.Constants.center)
 
         self.__bathroom_label = Label(self, text="Bath\nroom", bg="orange")
         self.__bathroom_label.grid(row=0, column=4, sticky=self.Constants.center)
-        self.__bathroom_toogle = ToggleButton(self, "Bathroom", action=self.__did_tap, tap_toggle_handler=tap_handler)
+        self.__bathroom_toogle = ToggleButton(self, "Bathroom", action=self.__did_tap, tap_toggle_handler= tap_handler)
         self.__bathroom_toogle.grid(row=1, column=4, sticky=self.Constants.center)
-
 
         self.grid_rowconfigure(0, weight=self.Constants.heigth//2)
         self.grid_rowconfigure(1, weight=self.Constants.heigth//2)
@@ -53,5 +51,10 @@ class MainView(Tk):
         self.grid_columnconfigure(4, weight=self.Constants.width//5)
 
     def __did_tap(self, sender):
-        if self.__tap_toogle_handler is None: return
-        self.__tap_toogle_handler(sender)
+        if self.__toogle_handler is None: return
+        if sender == "Bedroom One": id = 1
+        elif sender == "Bedroom Two": id = 2
+        elif sender == "Living room": id = 3
+        elif sender == "Dining room": id = 4
+        elif sender == "Bathroom": id = 5
+        self.__toogle_handler(id)
--- Views/ToggleButton.py
+++ Views/ToggleButton.py
@@ -7,7 +7,7 @@ class ToggleButton(Label):
         off_file = "Assets/OFF.ppm"
         event = "<Button-1>"
 
-    def __init__(self, master, room_name, action = None,  tap_toggle_handler = None):
+    def __init__(self, master, room_name, action = None, tap_toggle_handler = None):
         super().__init__(master)
         self.__tap_handler = tap_toggle_handler
         self.__room_name = room_name
@@ -26,7 +26,6 @@ class ToggleButton(Label):
         if self.__tap_handler is None: return
         self.__action(self.__room_name)
         self.__tap_handler(self.__state)
-        #print("{}, {}".format(self.__state, self.__room_name))
 
 
     def __set_image(self, image):
