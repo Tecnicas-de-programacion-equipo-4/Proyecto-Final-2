58397c1883948b5b40701f0800d6c998a3148d7a
luis M <lumarsa1712@gmail.com>
2017-11-26 17:24:50 -0600

    Added temperature control

--- Models/TemperatureControl.py
+++ Models/TemperatureControl.py
@@ -5,14 +5,16 @@ class TemperatureManager():
     class Constants:
         base_url = "http://api.openweathermap.org/data/2.5/weather?q=Mexico,MX&APPID={APIKEY}"
         kelvin_temperature = 273.15
+        main_key = "main"
+        temperature_key = "temp"
     @classmethod
     def get_temperature(cls):
         try:
             with request.urlopen(cls.Constants.base_url) as response:
                 data = response.read().decode()
                 json_data = json.loads(data)
-                variable_aux = json_data["main"]
-                temperature = float(variable_aux["temp"]) - cls.Constants.kelvin_temperature
+                variable_aux = json_data[cls.Constants.main_key]
+                temperature = float(variable_aux[cls.Constants.temperature_key]) - cls.Constants.kelvin_temperature
 
                 return temperature
         except Exception as error:
