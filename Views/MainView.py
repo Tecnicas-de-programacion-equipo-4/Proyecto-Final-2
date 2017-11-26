93f679da4a3568c54f575a553ded4623ccf8eff2
luis M <lumarsa1712@gmail.com>
2017-11-26 17:27:55 -0600

    Added ventilator 1 window

--- Views/MainView.py
+++ Views/MainView.py
@@ -1,24 +1,35 @@
-from tkinter import Tk, N, S, E, W
-from Views.Ventilator_Button import VentilatorButton
+from tkinter import Frame, Label, Button
 
-class MainView(Tk):
+from CustomType.View import View
+
+class StartView(Frame):
     class Constants:
-        title = "Toggle Ventilator"
-        heigth = 300
-        width = 300
-        center = N + S + E + W
+        label_text = "MainView Control de temperatura"
+        label_size = 10
+        ventilator_1_text = "Ventilator 1"
+        color = "blue"
+        color_font = "white"
 
-        @classmethod
-        def size(cls):
-            return "{}x{}".format(cls.width, cls.heigth)
+    def __init__(self, parent, change_view_hadler = None):
+        super().__init__(parent)
 
-    def __init__(self, tap_handler = None):
-        super().__init__()
-        self.title(self.Constants.title)
-        self.geometry(self.Constants.size())
+        self.__change_view_hadler = change_view_hadler
+        label = Label(self, text = self.Constants.label_text)
+        label.pack(pady = self.Constants.label_size, padx = self.Constants.label_size)
+        button = Button(self, text = self.Constants.ventilator_1_text,
+                           command=lambda: self.__did_tap_change_button(View.Ventilator_1))
+        button.pack()
 
-        self.__toogle = VentilatorButton(self, tap_toggle_handler = tap_handler)
-        self.__toogle.grid(row=0, column=0, sticky = self.Constants.center)
+        self.__temperature_label= Label(self)
 
-        self.grid_rowconfigure(0, weight = True)
-        self.grid_columnconfigure(0, weight = True)
+        self.__temperature_label.pack()
+        self.update_text("0")
+
+    def update_text(self, text):
+        self.__temperature_label.configure(text = text, bg = self.Constants.color, fg = self.Constants.color_font)
+
+
+    def __did_tap_change_button(self, view):
+        if self.__change_view_hadler is None:
+            return
+        self.__change_view_hadler(view)
---
+++ Views/ViewContainer.py
@@ -0,0 +1,23 @@
+from tkinter import Tk, Frame, N, S, E, W
+
+class MainView(Tk):
+    class Constants:
+        center = N + S + E + W
+        title = 'Automatic Mode'
+
+    def __init__(self):
+        super().__init__()
+        self.title(self.Constants.title)
+        self.__container = Frame(self)
+        self.__container.pack()
+        self.__container.grid_rowconfigure(0, weight=1)
+        self.__container.grid_columnconfigure(0, weight=1)
+
+    @property
+    def container (self):
+        return self.__container
+
+    def set_views (self, views):
+        for views in views:
+            views.grid(row = 0, column = 0, sticky =  self.Constants.center)
+
