b8d3117b6df103000ff6e6e5710ac0c967422e83
Alfonso <ponx.987>
2017-11-27 23:28:33 -0600

    add security functionality for windows

--- Views/MainView.py
+++ Views/MainView.py
@@ -1,11 +1,13 @@
-from tkinter import Tk, N, S, E, W, Label
+from tkinter import Tk, N, S, E, W, Label, Button
 from Views.ToggleButton import ToggleButton
+from smtplib import SMTP
+from correo import send_email, send_password
 #from Views.KeypadView import KeypadView
 class MainView(Tk):
     class Constants:
         title = "Casa Inteligente"
         heigth = 300
-        width = 375
+        width = 1000
         center = N + S + E + W
 
         @classmethod
@@ -22,8 +24,7 @@ class MainView(Tk):
 
         self.__bedroom_one_label = Label(self, text="Room\nOne", relief="sunken")
         self.__bedroom_one_label.grid(row=0, column=0, sticky=self.Constants.center)
-        self.__bedroom_one_toogle = ToggleButton(self, "Bedroom One", action=self.__did_tap,
-                                                 tap_toggle_handler=tap_handler)
+        self.__bedroom_one_toogle = ToggleButton(self, "Bedroom One", action=self.__did_tap, tap_toggle_handler=tap_handler)
         self.__bedroom_one_toogle.grid(row=1, column=0, sticky=self.Constants.center)
 
         self.__bedroom_two_label = Label(self, text="Room\nTwo", relief="sunken")
@@ -47,6 +48,33 @@ class MainView(Tk):
         self.__bathroom_toogle = ToggleButton(self, "Bathroom", action=self.__did_tap, tap_toggle_handler=tap_handler)
         self.__bathroom_toogle.grid(row=1, column=4, sticky=self.Constants.center)
 
+        self.is_active = False
+        self.current_pos = 0
+        self.inside_count = -1
+        self.outside_count = 0
+        self.inside_send = False
+        self.outside_send = False
+
+        self.__top_text = Label(self, text="Control de Alarma", bg="#666699", font=("Comic Sans MS", 30))
+        self.__activate = Button(self, text="Activar alarma", bg="black", fg="white", command=self.act, font=("Comic Sans MS", 18))
+        self.__outside_label = Label(self, text="Alarma", bg="#666699", font=("Comic Sans MS", 18))
+        self.__inside_label = Label(self, text="Desactivada", bg="#666699", font=("Comic Sans MS", 18))
+        self.__bottom_text = Label(self, text="Si se detectan parametros ariesgados se le notificara", font=("Comic Sans MS", 15))
+        self.__outside_value = Label(self, text=" ", font=("Comic Sans MS", 20), bg="#666699")
+        self.__inside_value = Label(self, text=" ", font=("Comic Sans MS", 20), bg="#666699")
+
+        self.__top_text.grid(row=0,column = 5, columnspan=2, sticky=self.Constants.center)
+        self.__activate.grid(row=1,column = 5, columnspan=2, sticky=self.Constants.center)
+        self.__outside_label.grid(row=2, column=5, sticky=self.Constants.center)
+        self.__inside_label.grid(row=2, column=6, sticky=self.Constants.center)
+        self.__outside_value.grid(row=3, column=5, sticky=self.Constants.center)
+        self.__inside_value.grid(row=3, column=6, sticky=self.Constants.center)
+        self.__bottom_text.grid(row=4, column = 5, columnspan=2, sticky=self.Constants.center)
+
+        for v in range(0, 2):
+            self.rowconfigure(v, weight=True)
+            self.columnconfigure(v, weight=True)
+
 
     def __configure_grid(self):
         self.grid_rowconfigure(0, weight=self.Constants.heigth//2)
@@ -62,3 +90,105 @@ class MainView(Tk):
         elif sender == "Dining room": id = 4
         elif sender == "Bathroom": id = 5
         self.__toogle_handler(id)
+
+    def act(self):
+        self.__top_text.config(text="Alarma Activa", bg="#000099")
+        self.__activate.config(text="Desactivar alarma", bg="red", command=self.deact)
+        self.__inside_label.config(text="Dentro de la casa", bg="black", fg="white")
+        self.__outside_label.config(text="Fuera de la casa", bg="white", fg="black")
+        self.is_active = True
+
+    def deact(self):
+        self.__top_text.config(text="Control de Alarma", bg="#666699")
+        self.__activate.config(text="Activar alarma", bg="black", command=self.act)
+        self.__inside_label.config(text="Desactivada", bg="#666699", fg="black")
+        self.__outside_label.config(text="Alarma", bg="#666699", fg="black")
+        self.__outside_value.config(text = " ", bg = "#666699")
+        self.__inside_value.config(text = " ", bg = "#666699")
+        self.is_active = False
+
+    def send_noti_out(self):
+        email = send_email
+        password = send_password
+        msj = """Se encontro movimiento sospechoso fuera de su casa
+
+        mas de 800 lecturas a menos de 50 cm de la entrada
+
+        se ha notificado a la policia"""
+
+        subject = "Alarma fuera de la casa"
+
+        email_msg = "Subject: {} \n\n {}".format(subject, msj)
+        smtp = SMTP("smtp.gmail.com", 587)
+
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(email, password)
+
+        smtp.sendmail(email, "ponx.987@gmail.com", email_msg)
+
+        smtp.quit()
+        print("Email sent")
+
+    def manage_outside(self,outs):
+        text = "{} cm".format(outs)
+        if int(outs) > 150:
+            self.__outside_value.config(text = text, bg="green")
+        if int(outs) in range(50,149):
+            self.__outside_value.config(text = text, bg="yellow")
+        if int(outs) < 50:
+            self.__outside_value.config(text = text, bg="red")
+            self.outside_count += 1
+            if self.outside_count > 500 and self.outside_send == False:
+                self.send_noti_out()
+                self.outside_count = -10000
+                self.outside_send = True
+
+    def send_noti_in(self):
+        email = send_email
+        password = send_password
+        msj = """Se encontro movimiento sospechoso dentro de su casa
+
+                  se ha notificado a la policia"""
+
+        subject = "Alarma dentro de la casa"
+
+        email_msg = "Subject: {} \n\n {}".format(subject, msj)
+        smtp = SMTP("smtp.gmail.com", 587)
+
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(email, password)
+
+        smtp.sendmail(email, "ponx.987@gmail.com", email_msg)
+
+        smtp.quit()
+        print("Email sent")
+
+    def manage_inside(self,ins):
+        move = abs(self.current_pos - int(ins))
+        text = "{} cm".format(move)
+        if move < 10:
+            self.__inside_value.config(text="leve "+text, bg="green")
+        if move in range(10,30):
+            self.__inside_value.config(text="moderado "+text, bg="yellow")
+            self.inside_count += 1
+        if move > 30:
+            self.__inside_value.config(text="grave " + text, bg="red")
+            self.inside_count += 2
+            if self.inside_count > 50 and self.inside_send == False:
+                self.send_noti_in()
+                self.inside_count = -10000
+                self.inside_send = True
+        self.current_pos = int(ins)
+
+    def sec(self, outs, ins):
+       if self.is_active == False:
+           pass
+       else:
+           self.manage_outside(outs)
+           self.manage_inside(ins)
--- main.py
+++ main.py
@@ -4,13 +4,28 @@ from serial.tools import list_ports
 
 class MainApp():
 
+    class Constants:
+        port = "COM3"
+        baud = 115200
+        close_event = "WM_DELETE_WINDOW"
+
     def __init__(self):
-        for port in list_ports.comports(include_links=True):
-            print(port.device, port.name, port.description)
 
         self.__master = MainView(toogle_handler = self.__handler_event, tap_handler = self.__toggle_did_change)
-        self.__arduino = serial.Serial('/dev/cu.usbmodem1411', 115200)
-        self.__master.protocol("WM_DELETE_WINDOW", self.__on_closing)
+        self.__arduino = serial.Serial(self.Constants.port, self.Constants.baud)
+        self.__master.protocol(self.Constants.close_event, self.__on_closing)
+        self.__update()
+
+    def __update(self):
+        data = self.__arduino.readline().decode()
+        clean_data = data.strip(" \n\r").split(",")
+        try:
+            outside = clean_data[0]
+            inside = clean_data[1]
+            self.__master.sec(outside, inside)
+        except:
+            pass
+        self.__master.after(1, self.__update)
 
     def run(self):
         self.__master.mainloop()
